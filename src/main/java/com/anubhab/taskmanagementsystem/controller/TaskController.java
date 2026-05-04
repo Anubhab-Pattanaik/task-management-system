3056debdba0046624425063145f1a79bb577a7e4
Anubhab-Pattanaik <anubhabpattanaik@gmail.com>
2026-05-04 21:29:34 +0100

    Added a new API to fetch details by ID

--- src/main/java/com/anubhab/taskmanagementsystem/controller/TaskController.java
+++ src/main/java/com/anubhab/taskmanagementsystem/controller/TaskController.java
@@ -1,11 +1,12 @@
 package com.anubhab.taskmanagementsystem.controller;
 
 import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.anubhab.taskmanagementsystem.dto.TaskResponseDTO;
+import com.anubhab.taskmanagementsystem.exception.ResourceNotFoundException;
 import com.anubhab.taskmanagementsystem.service.TaskService;
 
 @RestController
@@ -15,9 +16,13 @@ public class TaskController {
     private TaskService taskService;
     
     @GetMapping
-    public List<TaskResponseDTO> getAllTasks() {
+    public List<TaskResponseDTO> fetchAllTasks() {
         return taskService.getAllTasks();
     }
-    
+
+    @GetMapping("/{id}")
+    public TaskResponseDTO fetchTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id).orElseThrow(() -> new ResourceNotFoundException("Task with id " + id + " not found"));
+    }
 
 }
